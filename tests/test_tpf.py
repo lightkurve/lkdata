205b605d46e607cd2aa3f7da6a6d5c8d3576c1e1
Nschanche <nicole.e.schanche@nasa.gov>
2024-10-18 13:28:07 -0400

    ruff checks

---
+++ tests/test_tpf.py
@@ -0,0 +1,24 @@
+# import numpy as np
+# from lkdata.dataset import DataProcessorMixin, DataProducts, ErrorProducts, DataSet
+# from lkdata.datacube import DataCube, ErrorCube
+# from lkdata.dataframe import DataFrame, ErrorFrame
+# from lkdata.dataseries import DataSeries, ErrorSeries
+# from lkdata.tpf import TargetPixelFile
+
+
+def test_TPF_slicing():
+    # assert can slice in all dimensions, always returns a TPF!
+    pass
+
+
+# Write a new file of tests that should pass for TESS Target Pixel Files
+
+
+def test_SPOC_TPF():
+    # assert has poscorr
+    # assert has background
+    # assert has cadenceno
+    # assert has timecorr
+    # assert has CCD
+    # assert has camera
+    pass
