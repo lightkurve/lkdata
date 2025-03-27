3fed1137fac8410b27a4e24458744fc952c431e5
Daniel Giles <daniel.k.giles@gmail.com>
2025-03-27 16:18:45 -0700

    remove broken dataset (moved to new branch)

--- src/lkdata/__init__.py
+++ src/lkdata/__init__.py
@@ -5,14 +5,12 @@ from .version import __version__  # noqa: E402, F401
 from .datacube import DataCube, BoolCube, BitwiseCube  # noqa: F403, E402
 from .dataframe import DataFrame, BoolFrame, BitwiseFrame  # noqa: F403, E402
 from .dataseries import DataSeries, BoolSeries, BitwiseSeries  # noqa: F403, E402
-from .dataset import DataSet  # noqa: E402
 import os
 
 __all__ = [
     "DataCube",
     "DataFrame",
     "DataSeries",
-    "DataSet",
     "BoolCube",
     "BoolFrame",
     "BoolSeries",
