847183dd6a0f0b59653a42362e25285646445341
Daniel Giles <daniel.k.giles@gmail.com>
2025-08-20 17:54:43 -0400

    Correcting/simplifying dataseries documentation. Added default index handling

--- src/lkdata/dataseries.py
+++ src/lkdata/dataseries.py
@@ -45,6 +45,8 @@ class Series(
         pdseries = pd.Series(data)
         index = kwargs.pop("index", None)
         index_given = True if index is not None else False
+        if index_given and not isinstance(index, pd.Index):
+            index = pd.Index(index)
         index = self.parse_index(index, time_indices, pdseries.shape[0])
 
         """
@@ -166,103 +168,22 @@ class Series(
 
 
 class DataSeries(StatsMixin, Series):
-    """
-    pandas.Series-like object with uncertainty and lightkurve functionality.
-
-    pd.Series:
-    One-dimensional ndarray with axis labels (including time series).
-
-    Labels need not be unique but must be a hashable type. The object
-    supports both integer- and label-based indexing and provides a host of
-    methods for performing operations involving the index. Statistical
-    methods from ndarray have been overridden to automatically exclude
-    missing data (currently represented as NaN).
-
-    Operations between Series (+, -, /, \\*, \\*\\*) align values based on their
-    associated index values-- they need not be the same length. The result
-    index will be the sorted union of the two indexes.
+    """A pandas.Series-like object with uncertainty.
 
     Parameters
     ----------
     data : array-like, Iterable
         Contains data stored in Series. If data is a dict, argument order is
         maintained.
-    uncertainty : any type, optional
-        Uncertainty in the dataset. [Not a standard part of pd.Series]
-        Should have an attribute ``uncertainty_type`` that defines what kind of
-        uncertainty is stored, for example ``"std"`` for standard deviation or
-        ``"var"`` for variance. A metaclass defining such an interface is
-        `NDUncertainty` - but isn't mandatory. If the uncertainty has no such
-        attribute the uncertainty is stored as `StdDevm8Uncertainty`.
-        Defaults to ``None``.
+    uncertainty : array-like, optional
+        Uncertainty in the dataset.
     index : array-like or Index (1d)
-        Values must be hashable and have the same length as `data`.
-        Non-unique index values are allowed. Will default to
-        RangeIndex (0, 1, 2, ..., n) if not provided. If data is dict-like
-        and index is None, then the keys in the data are used as the index. If the
+        Will default to RangeIndex (0, 1, 2, ..., n) if not provided.
+        If data is dict-like and index is None, then the keys in the data are used as the index. If the
         index is not None, the resulting Series is reindexed with the index values.
     dtype : str, numpy.dtype, or ExtensionDtype, optional
         Data type for the output Series. If not specified, this will be
         inferred from `data`.
-
-    Examples
-    --------
-    Constructing Series from a dictionary with an Index specified
-
-    >>> d = {'a': 1, 'b': 2, 'c': 3}
-    >>> ser = pd.Series(data=d, index=['a', 'b', 'c'])
-    >>> ser
-    a   1
-    b   2
-    c   3
-    dtype: int64
-
-    The keys of the dictionary match with the Index values, hence the Index
-    values have no effect.
-
-    >>> d = {'a': 1, 'b': 2, 'c': 3}
-    >>> ser = pd.Series(data=d, index=['x', 'y', 'z'])
-    >>> ser
-    x   NaN
-    y   NaN
-    z   NaN
-    dtype: float64
-
-    Note that the Index is first build with the keys from the dictionary.
-    After this the Series is reindexed with the given Index values, hence we
-    get all NaN as a result.
-
-    Constructing Series from a list with `copy=False`.
-
-    >>> r = [1, 2]
-    >>> ser = pd.Series(r, copy=False)
-    >>> ser.iloc[0] = 999
-    >>> r
-    [1, 2]
-    >>> ser
-    0    999
-    1      2
-    dtype: int64
-
-    Due to input data type the Series has a `copy` of
-    the original data even though `copy=False`, so
-    the data is unchanged.
-
-    Constructing Series from a 1d ndarray with `copy=False`.
-
-    >>> r = np.array([1, 2])
-    >>> ser = pd.Series(r, copy=False)
-    >>> ser.iloc[0] = 999
-    >>> r
-    array([999,   2])
-    >>> ser
-    0    999
-    1      2
-    dtype: int64
-
-    Due to input data type the Series has a `view` on
-    the original data, so
-    the data is changed as well.
     """
 
     def __init__(self, data, uncertainty=None, index=None, dtype=None, **kwargs):
@@ -330,7 +251,7 @@ class BitwiseSeries(BitwiseMixin, Series):
     values_display : str, default "int"
         What kind of display should be used? Must be one of "int", "bitset", or
         "detailed". "detailed" requires code_reference to  be provided, or else
-        is identical to "parsed" which displays a set of the bits indicated by
+        is identical to "bitset" which displays a set of the bits indicated by
         the binary representation of the integer value.
     index : array-like or Index (1d)
         Values must be hashable and have the same length as `data`.
